5d377711db820a3633bb87acf485e9ddc5b9638f
Ricky Pinder <pinderrr@outlook.com>
2023-01-31 00:08:36 +0000

    removed calling loop in two locations

--- bin/main.ml
+++ bin/main.ml
@@ -7,14 +7,14 @@ let rec loop () =
     | Some x -> x
     | None -> ""
   in
-  match Diss_implementation.Parsing.attempt s with
+  (match Diss_implementation.Parsing.attempt s with
   | Some t ->
      let typeof = Diss_implementation.Interpreter.typeof (Diss_implementation.Environment.create ()) t in
      let t' = Diss_implementation.Interpreter.eval (Diss_implementation.Environment.create ()) t in
      let str = ((Diss_implementation.Interpreter.Terms.to_string t') ^ "\n" ^ (Diss_implementation.Interpreter.Typ.to_string typeof)) in
      
      Out_channel.output_string stdout (str ^ "\n");
-     loop ()
-  | None -> loop ()
+  | None -> ());
+  loop ()
 
 let () = loop ()
